0e3561ef6feb5b6344a78da3201e4bda422ce071
miguelzackk <mzsilvaw@gmail.com>
2024-10-07 21:59:52 -0300

    update-class-two

--- class-one.js
+++ class-one.js
@@ -1,143 +1,143 @@
-// // variables
-// // let nome = "nome"
-// // var temp = 'temporario'
-// // const constante = 'constante'
+// // // variables
+// // // let nome = "nome"
+// // // var temp = 'temporario'
+// // // const constante = 'constante'
 
-// // nome = 100
-// // temp = 200
+// // // nome = 100
+// // // temp = 200
 
-// // console.log(nome)
-// // console.log(temp)
-// // console.log(constante)
+// // // console.log(nome)
+// // // console.log(temp)
+// // // console.log(constante)
 
-// // console
-// // console.error('ERROR: TYPEERROR')
-// // console.info('servidor está rodando')
-// // console.table([
-// //     1, 2,3
-// // ])
-// // console.warn('lib antiga')
-// // console.time()
-// // console.clear()
+// // // console
+// // // console.error('ERROR: TYPEERROR')
+// // // console.info('servidor está rodando')
+// // // console.table([
+// // //     1, 2,3
+// // // ])
+// // // console.warn('lib antiga')
+// // // console.time()
+// // // console.clear()
 
-// // tipos de dados
+// // // tipos de dados
 
-// let name = 'Github'
-// const age = 40
-// let isApproved = false
-// let date = null
-// let address
+// // let name = 'Github'
+// // const age = 40
+// // let isApproved = false
+// // let date = null
+// // let address
 
-// const languages = ['java', 'javascript', 'php']
-// const numbers = [1, 2, 3]
+// // const languages = ['java', 'javascript', 'php']
+// // const numbers = [1, 2, 3]
 
-// numbers[3] = 'chocolate'
+// // numbers[3] = 'chocolate'
 
-// console.log(numbers, 'estou falando desse')
+// // console.log(numbers, 'estou falando desse')
 
-// const user = {
-//     email: 'teste@teste.com',
-//     password: 'teste123',
-//     agge: 10
-// }
+// // const user = {
+// //     email: 'teste@teste.com',
+// //     password: 'teste123',
+// //     agge: 10
+// // }
 
-// // desestruturacao
-// const { email, password } = user
+// // // desestruturacao
+// // const { email, password } = user
 
-// console.log(password, email, 'desestruturei o user')
+// // console.log(password, email, 'desestruturei o user')
 
-// console.log(user, 'user aqui')
-// // percorrer objeto
-// console.log(user.agge, 'user aqui')
-// console.log(user.email, 'user aqui')
-// console.log(user.password, 'user aqui')
+// // console.log(user, 'user aqui')
+// // // percorrer objeto
+// // console.log(user.agge, 'user aqui')
+// // console.log(user.email, 'user aqui')
+// // console.log(user.password, 'user aqui')
 
-// console.log(name)
-// console.log(age)
-// console.log(isApproved)
-// console.log(date)
-// console.log(address)
-// console.log(languages)
-// console.log(user)
+// // console.log(name)
+// // console.log(age)
+// // console.log(isApproved)
+// // console.log(date)
+// // console.log(address)
+// // console.log(languages)
+// // console.log(user)
 
-// // métodos string
+// // // métodos string
 
 // const fullName = 'Suporte de Domingo'
 
-// // metodo que conta caracteres - espaço tbm é um
-// const contaLetras = fullName.length
-// console.log(contaLetras)
+// // // metodo que conta caracteres - espaço tbm é um
+// // const contaLetras = fullName.length
+// // console.log(contaLetras)
 
-// const stringToArray = fullName.split(' ')
-// console.log(stringToArray)
+// // const stringToArray = fullName.split(' ')
+// // console.log(stringToArray)
 
-// const username = 'Ola, ' + stringToArray[0] + ' ' + stringToArray[2]
+// const username = 'Ola, ' + stringToArray[0] + ' ' + stringToArray[2]'
 // console.log(username)
 
-// // upperCase
-// const maiusculo = fullName.toUpperCase()
-// console.log(maiusculo)
+// // // upperCase
+// // const maiusculo = fullName.toUpperCase()
+// // console.log(maiusculo)
 
-// // lowerCase
-// const minusculo = fullName.toLowerCase()
-// console.log(minusculo)
+// // // lowerCase
+// // const minusculo = fullName.toLowerCase()
+// // console.log(minusculo)
 
-// // busca o indice
-// console.log(fullName.indexOf('de'), 'indexOf')
+// // // busca o indice
+// // console.log(fullName.indexOf('de'), 'indexOf')
 
-// // corta string por indice
-// console.log(fullName.slice(8, 10))
+// // // corta string por indice
+// // console.log(fullName.slice(8, 10))
 
-const remember = "Mariazinha da Silva Souza";
+// const remember = "Mariazinha da Silva Souza";
 
-console.log(remember.length, "remember");
+// console.log(remember.length, "remember");
 
-let arrayRemember = remember.split(" ");
+// let arrayRemember = remember.split(" ");
 
-console.log(arrayRemember[0], "slit");
-console.log(remember.slice(0, 10), "slice");
+// console.log(arrayRemember[0], "slit");
+// console.log(remember.slice(0, 10), "slice");
 
-console.table(arrayRemember);
-console.log(arrayRemember.length, "conta indice");
+// console.table(arrayRemember);
+// console.log(arrayRemember.length, "conta indice");
 
-const nameRule = arrayRemember[0] + ' ' + arrayRemember[arrayRemember.length - 1];
-console.log(nameRule)
+// const nameRule = arrayRemember[0] + ' ' + arrayRemember[arrayRemember.length - 1];
+// console.log(nameRule)
 
 
-console.log(arrayRemember[1], 'segundo item de um array')
+// console.log(arrayRemember[1], 'segundo item de um array')
 
-// usando metodo push
-arrayRemember.push('adicionei usando o método push')
-console.table(arrayRemember)
+// // usando metodo push
+// arrayRemember.push('adicionei usando o método push')
+// console.table(arrayRemember)
 
-// visualizar o ultimo item
-console.log(arrayRemember[arrayRemember.length -1   ])
+// // visualizar o ultimo item
+// console.log(arrayRemember[arrayRemember.length -1   ])
 
-// remover o ultimo item
-arrayRemember.pop()
-console.log(arrayRemember, 'pop')
+// // remover o ultimo item
+// arrayRemember.pop()
+// console.log(arrayRemember, 'pop')
 
-arrayRemember.shift()
-console.table(arrayRemember, 'shift')
+// arrayRemember.shift()
+// console.table(arrayRemember, 'shift')
 
-arrayRemember.unshift('novo item unshift')
-console.table(arrayRemember, 'unshift')
+// arrayRemember.unshift('novo item unshift')
+// console.table(arrayRemember, 'unshift')
 
-// metodos de objeto
-const product = {
-    name: 'Camisa',
-    price: 15.98,
-    inStock: true,
-    brand: 'Oakley'
-}
+// // metodos de objeto
+// const product = {
+//     name: 'Camisa',
+//     price: 15.98,
+//     inStock: true,
+//     brand: 'Oakley'
+// }
 
-console.log(product)
-console.log(product.name)
-console.log(product.brand)
+// console.log(product)
+// console.log(product.name)
+// console.log(product.brand)
 
-const { inStock } = product
+// const { inStock } = product
 
-console.log(inStock, 'usado a desetruturação do js')
+// console.log(inStock, 'usado a desetruturação do js')
 
-product.color = ['Azul', 'Vermelho', 'Verde']
-console.log(product.color)
+// product.color = ['Azul', 'Vermelho', 'Verde']
+// console.log(product.color)
---
+++ class-two.js
@@ -0,0 +1,45 @@
+// Estrutura condicionais
+
+// const a = 2
+
+// if (a > 8) {
+//     return console.log('O número é maior que 8')
+// } else {
+//     return console.log ('O número é menor que 8!')
+// }
+
+// const time = "Palmeiras";
+
+// else if
+// if (time === "Palmeiras") {
+//   console.log("O seu time é Palmeiras");
+// } else if (time === "São Paulo") {
+//   console.log("O seu time é São Paulo");
+// } else {
+//   console.log("Não encontramos seu time no nosso banco de dados");
+// }
+
+// ternário
+// const age = 121;
+
+// let ofGreater =
+//   age >= 18 ? console.log("Você é de maior") : console.log("Você é de menor");
+
+// // expressão com AND(e)
+// let onlyGreater = age >= 18 && console.log('mostra o site')
+
+// exercicio if
+// abaixo de 4 = reprovado
+// se a pessoa tirou 4 a 5 = recuperação
+// 6 a 9 = aprovado
+// 10 = excelente
+
+// const nota = 10
+//     if (nota < 4)
+//         console.log('Você está reprovado')
+//     else if(nota >= 4 && nota <= 5)
+//         console.log('Você está de recuperação')
+//     else if(nota >= 6 && nota <= 9)
+//         console.log('Você está aprovado')
+//     else (nota === 10)
+//         console.log("Seu desempenho é excelente")
