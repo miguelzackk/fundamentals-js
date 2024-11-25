06dea23fed067126dc731de3a8c8b02c8458de52
“miguelzackk” <mzsilvaw@gmail.com>
2024-11-25 20:06:00 -0300

    function static and dinamic

--- js/function.js
+++ js/function.js
@@ -4,26 +4,53 @@
 
 // Saudacao()
 
-function Somar(numOne, numTwo) {
-  return numOne + numTwo;
+// function Somar(numOne, numTwo) {
+//   return numOne + numTwo;
+// }
+
+// console.log(Somar(10, 30));
+
+// function Multiplicar(numOne, numTwo) {
+//   return numOne * numTwo;
+// }
+
+// console.log(Multiplicar(18, 10));
+
+// function MediaQuery() {
+//   const getWidhtScreen = window.innerWidth
+
+//   if (getWidhtScreen < 400) {
+//     console.log("é mobile")
+//   } else if (getWidhtScreen > 400 && getWidhtScreen < 780) {
+//     console.log("é tablet")
+//   } else {
+//     console.log("é desktop")
+//   }
+// }
+
+// MediaQuery()
+
+// FUNÇÕES ESTÁTICAS
+// function salve() {
+//     return console.log('salve, sou uma função estática')
+// }
+
+// salve()
+
+// function Saudaçao() {
+//     return "Olá, Mundo!"
+// }
+
+// console.log(Saudaçao())
+
+// function Somar(){
+//     return 2+2
+// }
+// console.log(Somar())
+
+
+// FUNÇÃO DINÂMICA
+function DynaSomar (a, b){
+    return a + b
 }
-
-console.log(Somar(10, 30));
-
-function Multiplicar(numOne, numTwo) {
-  return numOne * numTwo;
-}
-
-console.log(Multiplicar(18, 10));
-
-function MediaQuery(){
-    const getWidhtScreen = window.innerWidth
-
-    if(getWidhtScreen < 400) {
-        console.log('é mobile')
-    } else if (getWidhtScreen > 400 && getWidhtScreen < 780) {
-        console.log ('é tablet')
-    } else {
-        console.log('é desktop')
-    }
-}
+console.log(DynaSomar(10, 200))
