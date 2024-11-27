54698e72134fe05e7396f2402bb3ce6b0c4445a3
“miguelzackk” <mzsilvaw@gmail.com>
2024-11-27 20:59:44 -0300

    finish repository

--- js/f.j .js
+++ js/f.j .js
@@ -1,16 +1,37 @@
-// function Saudacao(name){
-//     const splitName = name.split(' ')
-//     const [firstName, ...rest] =splitName
-//     const lastName = rest[rest.length-1]
-
-//     return console.log (`Olá ${firstName} ${lastName}, Seja Bem-Vindo!`)
+// function banana (maca) {
+//     const splitName = maca.split(' ')
+//     const [pera, ...rest] = splitName
+//     const melao = rest[rest.length -1]
+//     return console.log(`Olá ${pera} ${melao}, Seja Bem-Vindo!`)
 // }
+// banana('Miguel Zacharias da Silva')
 
-// Saudacao('Miguel Zacharias da Silva')
 
-
-function IMC(a, b){
-    return a / (b ** 2);
+function calculo (peso, altura){
+    return peso / altura ** 2
 }
+console.log(`O seu IMC é ${calculo(60, 1.80)}.`)
 
-console.log(`O seu IMC é ${IMC(60, 1.80)}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- js/fun.js
+++ js/fun.js
@@ -1,3 +1,16 @@
-const fullname = 'Lucas do Espirito Santo'
+// const fullname = "Lucas do Espirito Santo";
 
-console.log(fullname.length, 'conta caracteres')
+// console.log(fullname.length, "conta caracteres");
+
+// const fruit = ["maça", "pessego", "uva"];
+// // console.log(fruit.length, 'array')
+// // console.table(fruit)
+// console.log(fruit[fruit.length -1])
+
+// const cortaNome = fullName.slice(1, 5)
+// console.log(cortaNome)
+
+const fullName = 'Miguel Zacharias da Silva'
+const transformToArray = fullName.split (' ')
+const saudacao = 'Olá, ' + transformToArray[0] + ' ' + transformToArray[transformToArray.length -1]
+console.log(saudacao)
