dd7f05ded453e1249f3dd4bd56bbce6f593fd178
“miguelzackk” <mzsilvaw@gmail.com>
2024-11-27 22:32:51 -0300

    calculo imc

--- js/f.j .js
+++ js/f.j .js
@@ -1,37 +1,39 @@
-// function banana (maca) {
-//     const splitName = maca.split(' ')
-//     const [pera, ...rest] = splitName
-//     const melao = rest[rest.length -1]
-//     return console.log(`Olá ${pera} ${melao}, Seja Bem-Vindo!`)
+// function banana(maca) {
+//   if (maca.length <= 0){
+//     return console.log("Insira um nome válido!");
 // }
-// banana('Miguel Zacharias da Silva')
+//   const splitName = maca.split(" ");
+//   const [pera, ...rest] = splitName;
+//   const melao = rest[rest.length - 1];
+//   return console.log(`Olá ${pera} ${melao}, Seja Bem-Vindo!`);
+// }
+// banana("");
 
-
-function calculo (peso, altura){
-    return peso / altura ** 2
+function calculo(peso, altura) {
+  return peso / altura ** 2;
 }
-console.log(`O seu IMC é ${calculo(60, 1.80)}.`)
+const imc = calculo(45, 1.70);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(`O seu IMC é ${calculo(45, 1.70).toFixed(2)}`)
+switch (true) {
+    case (imc < 18.5):
+      console.log('Você está abaixo do peso.');
+      break;
+    case (imc >= 18.5 && imc < 24.9):
+      console.log('Você está com peso normal.');
+      break;
+    case (imc >= 25 && imc < 29.9):
+      console.log('Você está com sobrepeso.');
+      break;
+    case (imc >= 30 && imc < 34.9):
+      console.log('Você está com obesidade grau 1.');
+      break;
+    case (imc >= 35 && imc < 39.9):
+      console.log('Você está com obesidade grau 2.');
+      break;
+    case (imc >= 40):
+      console.log('Você está com obesidade grau 3 (mórbida).');
+      break;
+    default:
+      console.log('Não foi possível classificar seu IMC.');
+  }
--- js/fun.js
+++ js/fun.js
@@ -10,7 +10,12 @@
 // const cortaNome = fullName.slice(1, 5)
 // console.log(cortaNome)
 
-const fullName = 'Miguel Zacharias da Silva'
-const transformToArray = fullName.split (' ')
-const saudacao = 'Olá, ' + transformToArray[0] + ' ' + transformToArray[transformToArray.length -1]
-console.log(saudacao)
+// const fullName = 'Miguel Zacharias da Silva'
+// const transformToArray = fullName.split (' ')
+// const saudacao = 'Olá, ' + transformToArray[0] + ' ' + transformToArray[transformToArray.length -1]
+// console.log(saudacao)
+
+function FirstName(fullName) {
+    return `Olá, ${fullName.split(' ')[0]} tudo bem?`
+}
+console.log(FirstName('Mauricio de Souza'))
